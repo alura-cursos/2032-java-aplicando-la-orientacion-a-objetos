ec8739a831425a6eb9c30b4ad768052e67dae0e1
genesysaluralatam <genessys.rondon@alura.com.br>
2023-09-07 11:35:41 -0300

    aula 2

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,20 +1,61 @@
 package com.aluracursos.screenmatch.modelos;
 
-import com.aluracursos.screenmacth.calculos.Clasificable;
+public class Pelicula {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDelasEvaluaciones;
 
-public class Pelicula extends Titulo implements Clasificable {
-    private String director;
-
-    public String getDirector() {
-        return director;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
     }
 
-    @Override
-    public int getClasificacion() {
-        return (int) (calculaMedia() / 2);
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDelasEvaluaciones(){
+        return totalDelasEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: "+ getDuracionEnMinutos());
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDelasEvaluaciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDelasEvaluaciones;
     }
 }
